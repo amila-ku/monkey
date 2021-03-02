8252d762daaebf9757285f4278735b1f45bbfa97
amila-ku <amila.kumaranayaka@gmail.com>
2021-03-02 23:19:19 +0100

    add ast content

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,10 @@
 // Package ast(Abstract Syntax Tree)  parser starts here
 package ast
 
+import (
+	"github.com/amila-ku/monkey/token"
+)
+
 type Node interface {
 	TokenLiteral() string
 
@@ -15,3 +19,37 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+		
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET
+	Name *Identifier
+	Value Expression 
+}
+
+func (ls *LetStatement) statementNode() {
+
+}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode(){}
