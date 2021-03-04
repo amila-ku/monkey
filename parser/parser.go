3a0267f4a8ab0d62fc8ac412cfb78e1ffc5f0a30
amila-ku <amila.kumaranayaka@gmail.com>
2021-03-04 23:15:14 +0100

    adding parser

---
+++ parser/parser.go
@@ -0,0 +1,27 @@
+package parser
+
+import (
+	"github.com/amila-ku/monkey/lexer"
+	"github.com/amila-ku/monkey/token"
+	"github.com/amila-ku/monkey/ast"
+)
+
+type Parser struct {
+	curToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// read token twise to set curToken and peekToken
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func(p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
