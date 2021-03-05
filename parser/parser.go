416b64ee43173ebe2d21091a5fc0b61366641bc8
amila-ku <amila.kumaranayaka@gmail.com>
2021-03-05 07:22:42 +0100

    parse function

--- parser/parser.go
+++ parser/parser.go
@@ -25,3 +25,8 @@ func(p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 }
+
+//ParseProgram parses code and returns ast
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
